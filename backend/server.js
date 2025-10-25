0a29525ae5156a66d0f538d12a4c71d29c39237b
Oumar Dimnang <oumar.dimnang@gmail.com>
2025-10-25 23:30:56 +0300

    Added Host

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,8 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
+const HOST = "0.0.0.0";
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, HOST, () => {
+  console.log(`Server is running on http://${HOST}:${PORT}`);
 });
